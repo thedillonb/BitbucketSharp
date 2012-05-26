dde9a230156912c59d0e8bd89ea9a1b654183a40
Dillon Buchanan <thedillonb@gmail.com>
2012-05-26 17:47:43 -0400

    Initial Commit!

---
+++ Client.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BitBucketSharp.Objects;
+using RestSharp;
+using RestSharp.Deserializers;
+
+namespace BitBucketSharp
+{
+    public class Client
+    {
+        private readonly RestClient _client = new RestClient("https://api.bitbucket.org/1.0");
+
+        /// <summary>
+        /// Gets the username for this clietn
+        /// </summary>
+        public String Username { get; private set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        public Client(String username, String password)
+        {
+            Username = username;
+            _client.Authenticator = new HttpBasicAuthenticator(username, password);
+        }
+
+        public Emails GetMyEmails()
+        {
+            return Get<Emails>("emails");
+        }
+
+        public List<RepositoryDetailed> GetRepositoriesIFollow()
+        {
+            return Get<List<RepositoryDetailed>>("user/follows");
+        }
+
+        public Users GetUsers(String username)
+        {
+            return Get<Users>("users/" + Username);
+        }
+
+        public FollowersObject GetUserFollowers(String username)
+        {
+            return Get<FollowersObject>("users/" + username + "/followers");
+        }
+
+        public FollowersObject GetRepositoryFollowers(String repoOwner, String repoSlug)
+        {
+            return Get<FollowersObject>("repositories/" + repoOwner + "/" + repoSlug + "/followers");
+        }
+
+        public FollowersObject GetIssueFollowers(String repoOwner, String repoSlug, int issueId)
+        {
+            return Get<FollowersObject>("repositories/" + repoOwner + "/" + repoSlug + "/issues/" + issueId + "/followers");
+        }
+
+        /// <summary>
+        /// Makes a 'GET' request to the server using an URI
+        /// </summary>
+        /// <typeparam name="T">The type of object the response should be deserialized ot</typeparam>
+        /// <param name="uri">The URI to request information from</param>
+        /// <returns>An object with response data</returns>
+        private T Get<T>(String uri)
+        {
+            var request = new RestRequest(uri);
+            var response = _client.Execute(request);
+            var d = new JsonDeserializer();
+            return d.Deserialize<T>(response);
+        }
+    }
+}
