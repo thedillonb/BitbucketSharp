74a738249f2fa5ef8f098bc6970de467d5a34697
Dillon Buchanan <thedillonb@gmail.com>
2012-11-15 01:16:42 -0500

    Added more event names

--- BitbucketSharp/Models/EventModel.cs
+++ BitbucketSharp/Models/EventModel.cs
@@ -22,13 +22,35 @@ namespace BitbucketSharp.Models
         public static class Type
         {
             public static readonly string 
-                Commit = "commit", CreateRepo = "create",
-                DeleteRepo = "delete", 
+
+                //Repository
+                Commit = "commit", CreateRepo = "create", DeleteRepo = "delete", StripRepo = "strip", ForkRepo = "fork", PatchQueue = "mq",
+
+                //Wiki
                 WikiCreated = "wiki_created", WikiUpdated = "wiki_updated",
+
+                //Following
                 StartFollowUser = "start_follow_user", StopFollowUser = "stop_follow_user",
                 StartFollowRepo = "start_follow_repo", StopFollowRepo = "stop_follow_repo",
-                IssueReported = "report_issue", IssueUpdated = "issue_update",
-                IssueComment = "issue_comment";
+                StartFollowIssue = "start_follow_issue", StopFollowIssue = "stop_follow_issue",
+
+                //Issues
+                IssueReported = "report_issue", IssueUpdated = "issue_update", IssueComment = "issue_comment",
+
+                //File
+                FileUploaded = "file_uploaded", 
+
+                //Pull Requests
+                PullRequestCreated = "pullrequest_created", PullRequestUpdated = "pullrequest_updated",
+                PullRequestFulfilled = "pullrequest_fulfilled", PullRequestRejected = "pullrequest_rejected",
+                PullRequestSuperseded = "pullrequest_superseded", PullRequestCommentCreated = "pullrequest_comment_created",
+                PullRequestCommentUpdated = "pullrequest_comment_updated", PullRequestCommentDeleted = "pullrequest_comment_deleted",
+                PullRequestLike = "pullrequest_like", PullRequestUnlike = "pullrequest_unlike",
+
+                //ChangeSet
+                ChangeSetCommentCreated = "cset_comment_created", ChangeSetCommentUpdated = "cset_comment_updated", 
+                ChangeSetCommentDeleted = "cset_comment_deleted",
+                ChangeSetLike = "cset_like", ChangeSetUnlike = "cset_unlike";
         }
     }
 }
